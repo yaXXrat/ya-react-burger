569223dbe1fe7d51e4de09998d10efb1f6c15bac
Artem <yaxxrat@yandex.ru>
2021-11-18 22:18:36 +0700

    1. Missed props for MenuItem 2. Code clean-up

--- src/components/modal-overlay/modal-overlay.js
+++ src/components/modal-overlay/modal-overlay.js
@@ -1,31 +1,9 @@
-import { useEffect } from "react";
 import style from "./modal-overlay.module.css";
 import PropTypes from "prop-types";
 
 const ModalOverlay = ({ onClose }) => {
-  // useEffect(() => {
-  //   window.addEventListener("keydown", onClose);
-  //   return () => {
-  //     window.removeEventListener("keydown", onClose);
-  //   };
-  // }, [onClose]);
-
-  useEffect(() => {
-    window.addEventListener(",,mouse", onClose);
-    return () => {
-      window.removeEventListener("keydown", onClose);
-    };
-  }, [onClose]);
-
-
-  // const handleKeyDown = (e) => {
-  //   if (e.keyCode === 27) {
-  //     onClose();
-  //   }
-  // };
 
   const onModalClose = (e) => {
-    console.log(e);
     if (e.target === e.currentTarget) {
       onClose(e);
     }
@@ -35,7 +13,6 @@ const ModalOverlay = ({ onClose }) => {
     <div
       className={style.open}
       onClick={(e) => onModalClose(e)}
-      // onKeyDown={handleKeyDown}
     />
   );
 };
