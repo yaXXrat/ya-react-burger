3bef6f9365a4044ce03a9bee8c3f24d269431279
Artem <yaxxrat@yandex.ru>
2021-12-21 21:29:09 +0700

    Next part pf auth service: working with access and refresh tokens

--- src/services/auth.js
+++ src/services/auth.js
@@ -7,13 +7,17 @@ const authRequest = async (endpoint, body, method) => {
             method,
             headers: { "Content-Type": "application/json" },
         }
+        const accessToken = getAccessToken()
+        if (accessToken) {
+            options.headers.authorization = accessToken
+        }
+
         if (body) {
             options.body = JSON.stringify(body)
         }
 
         const response = await fetch(`${SERVER_API_URL}${endpoint}`, options)
         result = await response.json()
-
     }
     catch (e) {
         result = { success: false, message: e.name + ' ' + e.message }
@@ -39,5 +43,71 @@ export const resetPassword = async email => postRequest('password-reset', { emai
 
 export const updatePassword = async (password, token) => postRequest('password-reset/reset', { password, token })
 
+export const registerUser = async (name, email, password) => {
+    const result = await postRequest('auth/register', { name, email, password })
+    if (result.success) {
+        setAccessToken(result.accessToken)
+        setRefreshToken(result.refreshToken)
+    }
+    return result
+}
 
-export const registerUser = async (name, email, password) => postRequest('auth/register', { name, email, password })
+export function getCookie(name) {
+    let matches = document.cookie.match(
+        // eslint-disable-next-line
+        new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)")
+    );
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+export function setCookie(name, value, props) {
+    props = props || {};
+    let exp = props.expires;
+    if (typeof exp == 'number' && exp) {
+      const d = new Date();
+      d.setTime(d.getTime() + exp * 1000);
+      exp = props.expires = d;
+    }
+    if (exp && exp.toUTCString) {
+      props.expires = exp.toUTCString();
+    }
+    value = encodeURIComponent(value);
+    let updatedCookie = name + '=' + value;
+    for (const propName in props) {
+      updatedCookie += '; ' + propName;
+      const propValue = props[propName];
+      if (propValue !== true) {
+        updatedCookie += '=' + propValue;
+      }
+    }
+    document.cookie = updatedCookie;
+}
+  
+export function deleteCookie(name) {
+    setCookie(name, "", {
+      'max-age': -1
+    })
+}
+
+export const getAccessToken = () => { getCookie('accessToken') }
+
+
+const setAccessToken = (accessToken) => {
+  if (accessToken) {
+      setCookie('accessToken', accessToken)
+  }
+  else {
+      deleteCookie('accessToken')
+  }
+}
+
+export const getRefreshToken = () => { getCookie('refreshToken') }
+
+const setRefreshToken = (refreshToken) => {
+    if (refreshToken) {
+        setCookie('refreshToken', refreshToken)
+    }
+    else {
+        deleteCookie('refreshToken')
+    }
+}
