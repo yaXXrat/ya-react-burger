056381d65a8241fe1e32ce992d3b045ecb90b569
xxrat <64155049+xxrat@users.noreply.github.com>
2021-12-24 10:48:42 +0700

    Forgot pwd page

--- src/pages/forgot-password-page.js
+++ src/pages/forgot-password-page.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import { forgot } from '../services/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { EmailInput, Button}  from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+    Link, useHistory
+} from "react-router-dom";
+import style from "./shared.module.css";
+import { RESET_ERROR_MESSAGE } from '../services/actions/error';
+import Modal from '../components/modal/modal';
+import DisplayError from "../components/display-error/display-error";
 
 function ForgotPassPage() {
+    const history = useHistory()
+
+    const [userEmail, setUserEmail] = useState('')
+
+    const { errorMessage }  = useSelector(store => store.errorInfo);
+    const { isLogged, user } = useSelector(store => store.auth);
+
+    useEffect(() => {
+        if(isLogged) {
+            history.push("/")
+        }
+    }, [isLogged, history, user]);
+
+    const dispatch = useDispatch();
+
+    const onSubmitLoginForm = async (e) => {
+        e.preventDefault()
+        dispatch(forgot(userEmail))
+    }
+
+    const hideDisplayError = () => {
+        dispatch({type: RESET_ERROR_MESSAGE});
+    };
+
     return (
-        <div>
-            <h1>Forgot pass page</h1>
-        </div>
+        <>
+            <form
+                className={style.form_block}
+                onSubmit={onSubmitLoginForm}>
+                <h2 className='mt-30 text text_type_main-medium'>Восстановление пароля</h2>
+
+                <div className='mt-6 mb-6'>
+                    <EmailInput
+                        onChange={(e) => setUserEmail(e.target.value)}
+                        name='email'
+                        value={userEmail}
+                    />
+                </div>
+                <Button>Восстановить</Button>
+
+                <div className='mt-25 text text_type_main-small text_color_inactive'>Вспомнили пароль?<Link className={`${style.link} pl-2`} to="/login">Войти</Link></div>
+            </form>
+            { errorMessage && (
+                <Modal onClose={hideDisplayError} className={style['error-modal']}>
+                    <DisplayError />
+                </Modal>
+            )}
+        </>
     )
 }
 
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,16 @@
 import { SERVER_API_URL } from './config';
-import { REGISTER_REQUEST, REGISTER_SUCCESS, REGISTER_ERROR, LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_ERROR } from './actions/auth';
-import { SET_ERROR_MESSAGE } from './actions/error'
+import {
+    REGISTER_REQUEST,
+    REGISTER_SUCCESS,
+    REGISTER_ERROR,
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS,
+    LOGIN_ERROR,
+    FORGOT_PASS_REQUEST,
+    FORGOT_PASS_SUCCESS,
+    FORGOT_PASS_ERROR
+} from './actions/auth';
+import {RESET_ERROR_MESSAGE, SET_ERROR_MESSAGE} from './actions/error'
 
 export function registerUser(name, email, password){
     return function(dispatch) {
@@ -79,6 +89,44 @@ export function login(email, password){
             dispatch({type: SET_ERROR_MESSAGE, errorMessage: e.name+ ' ' + e.message});
 
         })
+    };
+}
+
+
+export function forgot(email){
+    return function(dispatch) {
+        dispatch({type:FORGOT_PASS_REQUEST});
+        fetch(
+            SERVER_API_URL+'password-reset',
+            {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({email})
+            }
+        )
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error("Error happened during data fetching while reset! " + response.status);
+                }
+            })
+            .then((result) => {
+                if(result.success){
+                    dispatch({type:FORGOT_PASS_SUCCESS, data: result});
+                    console.log(result)
+                } else {
+                    throw new Error("Error happened during reset!");
+                }
+            })
+            .catch((e) => {
+                dispatch({type: FORGOT_PASS_ERROR});
+                dispatch({type: RESET_ERROR_MESSAGE, errorMessage: e.name+ ' ' + e.message});
+
+            })
     };
 }
 
