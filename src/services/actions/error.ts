367759bb4496f0191a77ca2d630b389ce3c725c8
Artem <yaxxrat@yandex.ru>
2022-01-29 13:46:02 +0700

    Redux typification: first step. error management

---
+++ src/services/actions/error.ts
@@ -0,0 +1,22 @@
+import { SET_ERROR_MESSAGE, RESET_ERROR_MESSAGE } from "../constants/error";
+
+export interface ISetErrorMessageAction {
+    readonly type: typeof SET_ERROR_MESSAGE;
+    readonly text: string;
+  }
+export interface IResetErrorMessageAction {
+    readonly type: typeof RESET_ERROR_MESSAGE;
+  }
+
+export type TErrorActions = 
+    | ISetErrorMessageAction
+    | IResetErrorMessageAction;  
+
+
+export const setErrorMessage = (text: string): TErrorActions => ({
+    type: SET_ERROR_MESSAGE,
+    text
+  });    
+export const resetErrorMessage = (): TErrorActions => ({
+    type: RESET_ERROR_MESSAGE
+  });      
---
+++ src/services/reducers/error.ts
@@ -0,0 +1,32 @@
+import type { TErrorActions } from '../actions/error';
+
+import { 
+    SET_ERROR_MESSAGE,
+    RESET_ERROR_MESSAGE
+} from '../constants/error';
+
+type TErrorState = {
+    errorMessage: string
+  } 
+
+export const initialState:TErrorState = {
+    errorMessage: ''
+};
+
+export const errorReducer = (state = initialState, action: TErrorActions) => {
+    switch (action.type) {
+        case SET_ERROR_MESSAGE:
+            return {
+                ...state,
+                errorMessage: action.text
+            }
+        case RESET_ERROR_MESSAGE:
+            return {
+                ...state,
+                errorMessage: ''
+            }
+        default: return state;
+    }
+}
+
+
