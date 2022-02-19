ee104322c0a3bf7f9a1ace3c23be91d29133c77f
Artem <yaxxrat@yandex.ru>
2022-02-19 12:28:32 +0700

    Updates

--- src/services/config.ts
+++
@@ -1,2 +0,0 @@
-export const SERVER_API_URL = 'https://norma.nomoreparties.space/api/';
-export const WS_API_URL = 'wss://norma.nomoreparties.space';
--- src/services/hooks.ts
+++
@@ -1,9 +0,0 @@
-import {
-    TypedUseSelectorHook,
-    useDispatch as dispatchHook,
-    useSelector as selectorHook
-} from 'react-redux';
-import {AppDispatch, AppThunk, RootState} from './types';
-
-export const useDispatch = () => dispatchHook<AppDispatch | AppThunk>();
-export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
